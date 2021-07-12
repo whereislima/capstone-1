cdf4c06c3d9a17a1bce925e9cccb732573bb3d66
whereislima <reach.lima@gmail.com>
2021-07-11 19:39:11 -0700

    implemented forms.py

--- forms.py
+++ forms.py
@@ -1,19 +1,7 @@
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, NumberField, FileField, SelectField
 from wtforms.validators import InputRequired, Length, Optional, Email
 from flask_wtf import FlaskForm
 
-
-class LoginForm(FlaskForm):
-    """Login form."""
-
-    username = StringField(
-        "Username",
-        validators=[InputRequired(), Length(min=1, max=20)],
-    )
-    password = PasswordField(
-        "Password",
-        validators=[InputRequired(), Length(min=6, max=55)],
-    )
 
 
 class RegisterForm(FlaskForm):
@@ -39,3 +27,27 @@ class RegisterForm(FlaskForm):
         "Last Name",
         validators=[InputRequired(), Length(max=30)],
     )
+
+class LoginForm(FlaskForm):
+    """Login form."""
+
+    username = StringField(
+        "Username",
+        validators=[InputRequired(), Length(min=1, max=20)],
+    )
+    password = PasswordField(
+        "Password",
+        validators=[InputRequired(), Length(min=6, max=55)],
+    )
+
+class ProfileForm(FlaskForm):
+    profile_image = FileField("profile image", Optional())
+    age = NumberField("age", validators=[InputRequired()])
+    skin_concerns = StringField("skin concerns", validators=[InputRequired()])
+
+class RoutineForm(FlaskForm):
+    time_of_day = SelectField()
+    step = NumberField()
+    type = SelectField()
+
+
